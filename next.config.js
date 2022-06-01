cad30d7d8705ceebab5f609a15779cf4eebf7f90
Adeleke Oluwatobi <tobiadiks@gmail.com>
2022-06-01 14:16:57 -0700

    added next image loader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withImageLoader=require('next-image-loader')
 module.exports = {
   reactStrictMode: true,
 }
+
+module.exports = withImageLoader({
+reactStrictMode:true,
+env:{
+  NEXT_PUBLIC_IMAGE_DOMAIN: 'https://res.cloudinary.com'
+}
+})
